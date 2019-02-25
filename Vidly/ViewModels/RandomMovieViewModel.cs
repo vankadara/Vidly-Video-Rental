951abb04e4af4b74a0244c3b5e1f3095933a3b74
vankadara <46946918+vankadara@users.noreply.github.com>
2019-02-25 13:46:38 -0600

    Add files via upload

---
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        //public type and random name
+        public Movie Movie{ get; set; }
+        //public type and random name
+        public List<Customer> Customers{ get; set; }
+    }
+}
